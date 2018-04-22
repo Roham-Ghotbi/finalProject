e4272fdfd0835ddcf3f214b8099a9e216ae342e8
mwaliman <mwaliman@berkeley.edu>
2018-04-22 15:21:06 -0700

    new action button class, project-line is working

---
+++ app/static/js/test.js
@@ -0,0 +1,13 @@
+$(".action-button").on('click', function() {
+        // move from list_todo container to list_doing container
+        console.log("Im in here");
+
+        // $(this).html("Add To To-Do");
+        if ($(this).hasClass("done")) {
+        	$(this).removeClass("done");
+	        $(this).addClass("not_done");
+        } else{
+        	$(this).removeClass("not_done");
+	        $(this).addClass("done");
+        }
+});
